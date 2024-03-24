c5167b38d8a8a64461e286c80886cdaceafb786d
did <didlohmann@hotmail.com>
2024-03-24 16:32:42 -0300

    remove funcao de retornarTudo

--- sistemas-operacionais/trabalhoFinal/client-server/client.js
+++ sistemas-operacionais/trabalhoFinal/client-server/client.js
@@ -10,22 +10,6 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const retornarTudo = async () => {
-    try {
-        const [response1, response2, response3] = await Promise.all([
-            axios.get(`http://localhost:${PORT_1}/`),
-            axios.get(`http://localhost:${PORT_2}/`),
-            axios.get(`http://localhost:${PORT_3}/`),
-        ]);
-
-        const serverData = [...response1.data, ...response2.data, ...response3.data];
-
-        console.log(serverData);
-    } catch (error) {
-        console.error('\nErro ao buscar dados do servidor:', error);
-    }
-};
-
 const buscaUnica = async (titulo) => {
     for (const serverUrl of [`http://localhost:${PORT_1}/`, `http://localhost:${PORT_2}/`, `http://localhost:${PORT_3}/`]) {
         try {
