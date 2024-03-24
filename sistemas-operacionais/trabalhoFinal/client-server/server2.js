490a09e5e5f318c4fbba194f65487563d69f3be3
Ingrid Lohmann <didlohmann@gmail.com>
2024-03-23 21:56:03 -0300

    ajusta busca de termo em servidores

--- sistemas-operacionais/trabalhoFinal/client-server/server2.js
+++ sistemas-operacionais/trabalhoFinal/client-server/server2.js
@@ -1,15 +1,38 @@
-const express = require('express');
-const { PORT_2 } = require('../utils');
-const { database2 } = require('../database');
+const express = require("express");
+const { PORT_2, STATUS_NOT_FOUND, normalize } = require("../utils");
+const { database2 } = require("../database");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.json(database2);
+app.get("/", (req, res) => {
+  res.json(database2);
+});
+
+app.get("/buscar/:key", async (req, res) => {
+  let foundData = null;
+
+  try {
+    const query = req.params.key; // Obter termo de busca da URL
+    const foundData = database2.filter(
+      (item) =>
+        normalize(item.titulo).includes(normalize(query)) ||
+        normalize(item.autor).includes(normalize(query))
+    ); // Filtrar dados
+    if (foundData) {
+      res.json(foundData);
+      return; // Encerra a busca após encontrar o dado
+    }
+  } catch (error) {
+    console.error("Erro ao buscar dados no servidor:", error);
+  }
+
+  if (!foundData) {
+    res.status(STATUS_NOT_FOUND).json({ message: "Dado não encontrado" });
+  }
 });
 
 app.listen(PORT_2, () => {
-   console.log(`Servidor 2 em execução na porta ${PORT_2}`);
+  console.log(`Servidor 2 em execução na porta ${PORT_2}`);
 });
